nxp-imx/uboot-imx

MLK-23949 imx8mn: evk: support jailhouse for lpddr4 board
Support jailhouse for lpddr4 board by add new env jh_root_dtb

Signed-off-by: Peng Fan <peng.fan@nxp.com>

include/configs/imx8mn_evk.h
@@ -79,14 +79,21 @@
  * Another approach is add the clocks for inmates into clks_init_on
  * in clk-imx8mm.c, then clk_ingore_unused could be removed.
  */
+#ifdef CONFIG_TARGET_IMX8MN_DDR4_EVK
+#define JH_ROOT_DTB	"imx8mn-ddr4-evk-root.dtb"
+#else
+#define JH_ROOT_DTB	"imx8mn-evk-root.dtb"
+#endif
+
 #define JAILHOUSE_ENV \
 	"jh_clk= \0 " \
-	"jh_mmcboot=mw 0x303d0518 0xff; setenv fdt_file imx8mn-ddr4-evk-root.dtb;" \
+	"jh_root_dtb=" JH_ROOT_DTB "\0" \
+	"jh_mmcboot=mw 0x303d0518 0xff; setenv fdt_file ${jh_root_dtb};" \
 		"setenv jh_clk clk_ignore_unused; " \
 			   "if run loadimage; then " \
 				   "run mmcboot; " \
 			   "else run jh_netboot; fi; \0" \
-	"jh_netboot=mw 0x303d0518 0xff; setenv fdt_file imx8mn-ddr4-evk-root.dtb; setenv jh_clk clk_ignore_unused; run netboot; \0 "
+	"jh_netboot=mw 0x303d0518 0xff; setenv fdt_file ${jh_root_dtb}; setenv jh_clk clk_ignore_unused; run netboot; \0 "
 
 #define CONFIG_MFG_ENV_SETTINGS \
 	CONFIG_MFG_ENV_SETTINGS_DEFAULT \

rafluan/u-boot-fslc

(cherry picked from commit a0e830308539e1afe19a0e15ea04e4ca341a1304)
(cherry picked from commit 0859642e0fe7cfdb89e3e37687c63b6b3161250a)
(cherry picked from commit 7e87f4e0dac53b77bb68c85a350d6a09cbe8b13c)
(cherry picked from commit e002fd25291e8b309087daa043b7e1e3cb873cd8)

include/configs/imx8mn_evk.h
@@ -30,14 +30,21 @@
  * Another approach is add the clocks for inmates into clks_init_on
  * in clk-imx8mm.c, then clk_ingore_unused could be removed.
  */
+#ifdef CONFIG_TARGET_IMX8MN_DDR4_EVK
+#define JH_ROOT_DTB	"imx8mn-ddr4-evk-root.dtb"
+#else
+#define JH_ROOT_DTB	"imx8mn-evk-root.dtb"
+#endif
+
 #define JAILHOUSE_ENV \
 	"jh_clk= \0 " \
-	"jh_mmcboot=mw 0x303d0518 0xff; setenv fdt_file imx8mn-ddr4-evk-root.dtb;" \
+	"jh_root_dtb=" JH_ROOT_DTB "\0" \
+	"jh_mmcboot=mw 0x303d0518 0xff; setenv fdtfile ${jh_root_dtb};" \
 		"setenv jh_clk clk_ignore_unused; " \
 			   "if run loadimage; then " \
 				   "run mmcboot; " \
 			   "else run jh_netboot; fi; \0" \
-	"jh_netboot=mw 0x303d0518 0xff; setenv fdt_file imx8mn-ddr4-evk-root.dtb; setenv jh_clk clk_ignore_unused; run netboot; \0 "
+	"jh_netboot=mw 0x303d0518 0xff; setenv fdtfile ${jh_root_dtb}; setenv jh_clk clk_ignore_unused; run netboot; \0 "
 
 #define CFG_MFG_ENV_SETTINGS \
 	CFG_MFG_ENV_SETTINGS_DEFAULT \