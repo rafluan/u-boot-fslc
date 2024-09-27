nxp-imx/uboot-imx

MLK-23834: Add snvs_security to 8dxl
Add the support for the snvs_security module on all 8dxl
SoC.

Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>
(cherry picked from commit b928877399a6ca3d0ff0af5c9c136f8dee3f150d)

board/freescale/imx8dxl_evk/imx8dxl_evk.c
@@ -16,6 +16,7 @@
 #include <asm/arch/clock.h>
 #include <asm/arch/sci/sci.h>
 #include <asm/arch/imx8-pins.h>
+#include <asm/arch/snvs_security_sc.h>
 #include <asm/arch/iomux.h>
 #include <asm/arch/sys_proto.h>
 #include <usb.h>
@@ -177,6 +178,15 @@ int board_init(void)
 #ifdef CONFIG_DWC_ETH_QOS
 	/* clock, phy interface mode */
 	setup_eqos();
+#endif
+
+#ifdef CONFIG_SNVS_SEC_SC_AUTO
+	{
+		int ret = snvs_security_sc_init();
+
+		if (ret)
+			return ret;
+	}
 #endif
 
 	return 0;

rafluan/u-boot-fslc

(cherry picked from commit 7a1e94feef7b746b8d58d090a7a8edd55c472946)
(cherry picked from commit 4bfc235589283d0950a6e42c3ab867773db5c2a9)
(cherry picked from commit 0b24f49be966839d3d28e9b48296a2e32b503e2d)
(cherry picked from commit 138da8739aff3d260018594c14fabca12d183c0a)

board/freescale/imx8dxl_evk/imx8dxl_evk.c
@@ -18,6 +18,7 @@
 #include <asm/arch/clock.h>
 #include <firmware/imx/sci/sci.h>
 #include <asm/arch/imx8-pins.h>
+#include <asm/arch/snvs_security_sc.h>
 #include <asm/arch/iomux.h>
 #include <asm/arch/sys_proto.h>
 #include <usb.h>
@@ -159,6 +160,15 @@ int board_init(void)
 #ifdef CONFIG_DWC_ETH_QOS
 	/* clock, phy interface mode */
 	setup_eqos();
+#endif
+
+#ifdef CONFIG_IMX_SNVS_SEC_SC_AUTO
+	{
+		int ret = snvs_security_sc_init();
+
+		if (ret)
+			return ret;
+	}
 #endif
 
 	return 0;