nxp-imx/uboot-imx

MLK-23241 imx8dx_mek: Add iMX8DX MEK board support
iMX8DX MEK board has similar design with 8QXP MEK. The major changes are
1. DDR changed to 16bits 1GB DDR part
2. USB3.0 is removed and only support OTG on typec port. (No SW change needed)

This patch adds new defconfigs and DTS file for this new board.

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>
(cherry picked from commit 5efb4275f6cfefb5dd342f2e498834b40b989883)

board/freescale/imx8qxp_mek/imx8qxp_mek.c
@@ -349,7 +349,11 @@ static void board_gpio_init(void)
 
 int checkboard(void)
 {
+#ifdef CONFIG_TARGET_IMX8DX_MEK
+	puts("Board: iMX8DX MEK\n");
+#else
 	puts("Board: iMX8QXP MEK\n");
+#endif
 
 	print_bootinfo();
 
@@ -657,7 +661,11 @@ int board_late_init(void)
 
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 	env_set("board_name", "MEK");
+#ifdef CONFIG_TARGET_IMX8DX_MEK
+	env_set("board_rev", "iMX8DX");
+#else
 	env_set("board_rev", "iMX8QXP");
+#endif
 #endif
 
 	env_set("sec_boot", "no");
@@ -669,10 +677,17 @@ int board_late_init(void)
 	m4_boot = check_m4_parts_boot();
 
 	if (fdt_file && !strcmp(fdt_file, "undefined")) {
+#ifdef CONFIG_TARGET_IMX8DX_MEK
+		if (m4_boot)
+			env_set("fdt_file", "fsl-imx8dx-mek-rpmsg.dtb");
+		else
+			env_set("fdt_file", "fsl-imx8dx-mek.dtb");
+#else
 		if (m4_boot)
 			env_set("fdt_file", "fsl-imx8qxp-mek-rpmsg.dtb");
 		else
 			env_set("fdt_file", "fsl-imx8qxp-mek.dtb");
+#endif
 	}
 
 #ifdef CONFIG_ENV_IS_IN_MMC

rafluan/u-boot-fslc

(cherry picked from commit 8185fa9fa8e48d64d4abf8066bf080f02343d484)
(cherry picked from commit c239a08e35459f0372d1f4b4a8366525bfdef6db)
(cherry picked from commit 3d7c327d865eaa2f3a0aa313ecc34bf2ba4b1b60)
(cherry picked from commit 0b582840a005f9fe9e29aa7a7620d8f5fff37261)

board/freescale/imx8qxp_mek/imx8qxp_mek.c
@@ -92,7 +92,11 @@ static inline void board_gpio_init(void) {}
 
 int checkboard(void)
 {
+#ifdef CONFIG_TARGET_IMX8DX_MEK
+	puts("Board: iMX8DX MEK\n");
+#else
 	puts("Board: iMX8QXP MEK\n");
+#endif
 
 	build_info();
 	print_bootinfo();
@@ -260,8 +264,13 @@ int board_late_init(void)
 
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 	env_set("board_name", "MEK");
+#ifdef CONFIG_TARGET_IMX8DX_MEK
+	env_set("board_rev", "iMX8DX");
+#else
 	env_set("board_rev", "iMX8QXP");
 #endif
+#endif
+
 	env_set("sec_boot", "no");
 #ifdef CONFIG_AHAB_BOOT
 	env_set("sec_boot", "yes");
@@ -271,10 +280,17 @@ int board_late_init(void)
 	m4_booted = m4_parts_booted();
 
 	if (fdt_file && !strcmp(fdt_file, "undefined")) {
+#ifdef CONFIG_TARGET_IMX8DX_MEK
+		if (m4_booted)
+			env_set("fdt_file", "imx8dx-mek-rpmsg.dtb");
+		else
+			env_set("fdt_file", "imx8dx-mek.dtb");
+#else
 		if (m4_booted)
 			env_set("fdt_file", "imx8qxp-mek-rpmsg.dtb");
 		else
 			env_set("fdt_file", "imx8qxp-mek.dtb");
+#endif
 	}
 
 #ifdef CONFIG_ENV_IS_IN_MMC