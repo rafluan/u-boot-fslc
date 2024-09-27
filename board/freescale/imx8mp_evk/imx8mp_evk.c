nxp-imx/uboot-imx

MLK-24381 imx8mp_evk: Reserve Mcore vring/buffer memory
When running with OPTEE, reserve 16MB memory for Mcore vring/buffer.
After u-boot bootaux Mcore, Mcore image will set its resources table
at vring address. Without the reservation, the u-boot runtime memory
may conflict with it.

Signed-off-by: Ye Li <ye.li@nxp.com>

board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -513,6 +513,17 @@ int board_late_init(void)
 	return 0;
 }
 
+#ifdef CONFIG_IMX_BOOTAUX
+ulong board_get_usable_ram_top(ulong total_size)
+{
+	/* Reserve 16M memory used by M core vring/buffer, which begins at 16MB before optee */
+	if (rom_pointer[1])
+		return gd->ram_top - SZ_16M;
+
+	return gd->ram_top;
+}
+#endif
+
 #ifdef CONFIG_FSL_FASTBOOT
 #ifdef CONFIG_ANDROID_RECOVERY
 int is_recovery_key_pressing(void)

rafluan/u-boot-fslc

(cherry picked from commit 333766be8f0e0b76ca5216ab64b5d55c4148d16d)
(cherry picked from commit 073d18ca9c77b38c751916bd885c59dd943c9543)
(cherry picked from commit d40e2a305e2e6153dd6424f8261c1562921f1e51)
(cherry picked from commit 16b719de45c1e824760d1e0693565bbe9cd05c6b)

board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -411,3 +411,14 @@ int board_late_init(void)
 
 	return 0;
 }
+
+#ifdef CONFIG_IMX_BOOTAUX
+phys_size_t board_get_usable_ram_top(phys_size_t total_size)
+{
+	/* Reserve 16M memory used by M core vring/buffer, which begins at 16MB before optee */
+	if (rom_pointer[1])
+		return gd->ram_top - SZ_16M;
+
+	return gd->ram_top;
+}
+#endif