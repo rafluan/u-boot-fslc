nxp-imx/uboot-imx

MLK-23964-12 video: Add iMX8MQ video drivers for HDMI display
Add DCSS video driver and HDMI display driver to adapt with HDP library

Signed-off-by: Ye Li <ye.li@nxp.com>

drivers/video/imx/hdmi/imx8m_hdmi.c
@@ -1,14 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright 2018 NXP
+ * Copyright 2019 NXP
  *
- * SPDX-License-Identifier:	GPL-2.0+
  */
 
 #include <common.h>
-#include <command.h>
-#include <asm/mach-imx/video.h>
-#include <asm/arch/video_common.h>
-#include <imx8_hdmi.h>
+#include <clk.h>
+#include <dm.h>
+#include <display.h>
+#include <video.h>
+#include <asm/io.h>
+#include <dm/device-internal.h>
+#include <linux/iopoll.h>
+#include <clk.h>
+#include <video_link.h>
 
 #include "API_General.h"
 #include "vic_table.h"
@@ -23,29 +28,19 @@
 #include "source_vif.h"
 #include "general_handler.h"
 #include "mhl_hdtx_top.h"
-
-
-#ifdef CONFIG_IMX8QM
-#include "API_AFE_mcu1_dp.h"
-#include "API_AFE_ss28fdsoi_kiran_hdmitx.h"
-#endif
-
-#ifdef CONFIG_IMX8M
 #include "API_AFE_t28hpc_hdmitx.h"
-#endif
 
-DECLARE_GLOBAL_DATA_PTR;
+struct imx8m_hdmi_priv {
+	fdt_addr_t base;
+	struct display_timing timings;
+	int vic;
+	bool hpol;
+	bool vpol;
+};
 
-#define ON  1
-#define OFF 0
-
-unsigned long g_encoding = 1;    /* 1 RGB, 2 YUV 444, 4 YUV 422, 8 YUV 420 */
-unsigned long g_color_depth = 8; /* 8 pits per color */
-
-static int imx8_hdmi_set_vic_mode(int vic,
-				  struct video_mode_settings *vms)
+static int imx8m_hdmi_set_vic_mode(int vic,
+				  struct imx8m_hdmi_priv *priv)
 {
-	/*struct video_mode_settings *vms = &vm_settings[VM_USER]; */
 	uint32_t pixel_clock_kHz;
 	uint32_t frame_rate_Hz;
 	uint32_t frame_rate_frac_Hz;
@@ -57,18 +52,17 @@ static int imx8_hdmi_set_vic_mode(int vic,
 		return -1;
 	}
 
+	priv->timings.hfront_porch.typ = vic_table[vic][FRONT_PORCH];
+	priv->timings.hback_porch.typ = vic_table[vic][BACK_PORCH];
+	priv->timings.hsync_len.typ = vic_table[vic][HSYNC];
+	priv->timings.vfront_porch.typ = vic_table[vic][TYPE_EOF];
+	priv->timings.vback_porch.typ = vic_table[vic][SOF];
+	priv->timings.vsync_len.typ = vic_table[vic][VSYNC];
+	priv->timings.hactive.typ = vic_table[vic][H_ACTIVE];
+	priv->timings.vactive.typ = vic_table[vic][V_ACTIVE];
 
-	vms->hfp   = vic_table[vic][FRONT_PORCH];
-	vms->hbp   = vic_table[vic][BACK_PORCH];
-	vms->hsync = vic_table[vic][HSYNC];
-	vms->vfp   = vic_table[vic][TYPE_EOF];
-	vms->vbp   = vic_table[vic][SOF];
-	vms->vsync = vic_table[vic][VSYNC];
-	vms->xres  = vic_table[vic][H_ACTIVE];
-	vms->yres  = vic_table[vic][V_ACTIVE];
-
-	vms->hpol = vic_table[vic][HSYNC_POL] != 0;
-	vms->vpol = vic_table[vic][VSYNC_POL] != 0;
+	priv->hpol = vic_table[vic][HSYNC_POL] != 0;
+	priv->vpol = vic_table[vic][VSYNC_POL] != 0;
 
 	cea_vic = vic_table[vic][VIC];
 	if (vic_table[vic][I_P] != 0)
@@ -80,40 +74,29 @@ static int imx8_hdmi_set_vic_mode(int vic,
 	frame_rate_frac_Hz = frame_rate_Hz % 1000;
 	frame_rate_Hz      /= 1000;
 
-	vms->pixelclock = pixel_clock_kHz;
+	priv->timings.pixelclock.typ = pixel_clock_kHz * 1000;
 
 	debug("Cadence VIC %3d, CEA VIC %3d: %4d x %4d %c @ %3d.%03d [%6d kHz] Vpol=%d Hpol=%d\n",
-	      vic, cea_vic, vms->xres, vms->yres, iflag, frame_rate_Hz,
-	      frame_rate_frac_Hz, pixel_clock_kHz, vms->vpol, vms->hpol);
+	      vic, cea_vic, priv->timings.hactive.typ, priv->timings.vactive.typ, iflag, frame_rate_Hz,
+	      frame_rate_frac_Hz, pixel_clock_kHz, priv->vpol, priv->hpol);
 
 	debug(" mode timing fp sync bp h:%3d %3d %3d v:%3d %3d %3d\n",
-	      vms->hfp, vms->hsync, vms->hbp, vms->vfp, vms->vsync, vms->vbp);
+	      priv->timings.hfront_porch.typ, priv->timings.hsync_len.typ, priv->timings.hback_porch.typ,
+	      priv->timings.vfront_porch.typ, priv->timings.vsync_len.typ, priv->timings.vback_porch.typ);
 
 	return 0;
-	/*debug("leaving %s() ...\n", __func__); */
 }
 
-static int imx8_hdmi_init(int vic,
+static int imx8m_hdmi_init(int vic,
 			  int encoding,
 			  int color_depth,
 			  bool pixel_clk_from_phy)
 {
 	int ret;
-#ifdef CONFIG_IMX8QM
-	sc_ipc_t ipcHndl = gd->arch.ipc_channel_handle;
-	void __iomem *hdmi_csr_base = (void __iomem *)0x56261000;
-#endif
-	/*GENERAL_Read_Register_response regresp; */
-	/*uint8_t sts; */
 	uint32_t character_freq_khz;
 
 	uint8_t echo_msg[] = "echo test";
 	uint8_t echo_resp[sizeof(echo_msg) + 1];
-	/*uint8_t response; */
-	/*uint8_t dpcd_resp; */
-	/*uint8_t hdcp_resp; */
-	/*uint8_t capb_resp; */
-	/*uint32_t temp; */
 
 	/*================================================================== */
 	/* Parameterization: */
@@ -147,22 +130,6 @@ static int imx8_hdmi_init(int vic,
 	/*================================================================== */
 	/* Parameterization done */
 	/*================================================================== */
-#ifdef CONFIG_IMX8QM
-	/* set the pixel link mode and pixel type */
-	SC_MISC_SET_CONTROL(ipcHndl, SC_R_HDMI, SC_C_PHY_RESET, 0);
-#if 1
-	SC_MISC_SET_CONTROL(ipcHndl, SC_R_DC_0, SC_C_PXL_LINK_MST1_ADDR, 1);
-	/*SC_MISC_SET_CONTROL(ipcHndl, SC_R_DC_0, SC_C_PXL_LINK_MST1_ADDR, 0);*/
-	if (g_clock_mode == CLOCK_MODES_HDMI_DUAL) {
-		SC_MISC_SET_CONTROL(ipcHndl, SC_R_DC_0,
-				    SC_C_PXL_LINK_MST2_ADDR, 2);
-		/*SC_MISC_SET_CONTROL(ipcHndl, SC_R_DC_0,
-		  SC_C_PXL_LINK_MST2_ADDR, 0); */
-		__raw_writel(0x6, hdmi_csr_base);
-	} else
-#endif
-		__raw_writel(0x34, hdmi_csr_base);
-#endif
 	cdn_api_init();
 	debug("CDN_API_Init completed\n");
 
@@ -185,27 +152,15 @@ static int imx8_hdmi_init(int vic,
 	debug("phy_cfg_t28hpc (character_freq_mhz = %d)\n",
 	      character_freq_khz);
 
-	/*phy_reset(1); */
-
-#ifdef CONFIG_IMX8QM
-	SC_MISC_SET_CONTROL(ipcHndl, SC_R_HDMI, SC_C_PHY_RESET, 1);
-#endif
 	hdmi_tx_t28hpc_power_config_seq(4);
-#ifdef CONFIG_IMX8QM
-	/* Set the lane swapping */
-	ret = cdn_api_general_write_register_blocking
-		(ADDR_SOURCD_PHY + (LANES_CONFIG << 2),
-		 F_SOURCE_PHY_LANE0_SWAP(3) | F_SOURCE_PHY_LANE1_SWAP(0) |
-		 F_SOURCE_PHY_LANE2_SWAP(1) | F_SOURCE_PHY_LANE3_SWAP(2) |
-		 F_SOURCE_PHY_COMB_BYPASS(0) | F_SOURCE_PHY_20_10(1));
-#else
+
 	/* Set the lane swapping */
 	ret = cdn_api_general_write_register_blocking
 		(ADDR_SOURCD_PHY + (LANES_CONFIG << 2),
 		 F_SOURCE_PHY_LANE0_SWAP(0) | F_SOURCE_PHY_LANE1_SWAP(1) |
 		 F_SOURCE_PHY_LANE2_SWAP(2) | F_SOURCE_PHY_LANE3_SWAP(3) |
 		 F_SOURCE_PHY_COMB_BYPASS(0) | F_SOURCE_PHY_20_10(1));
-#endif
+
 	debug("_General_Write_Register_blocking LANES_CONFIG ret = %d\n", ret);
 
 	ret = CDN_API_HDMITX_Init_blocking();
@@ -223,51 +178,30 @@ static int imx8_hdmi_init(int vic,
 	ret =  CDN_API_HDMITX_SetVic_blocking(vic_mode, bps, format);
 	debug("CDN_API_HDMITX_SetVic_blocking ret = %d\n", ret);
 
-#ifdef CONFIG_IMX8QM
-	{
-		GENERAL_Read_Register_response regresp;
-		/* adjust the vsync/hsync polarity */
-		cdn_api_general_read_register_blocking(ADDR_SOURCE_VIF +
-						       (HSYNC2VSYNC_POL_CTRL
-							<< 2),
-						       &regresp);
-		debug("Initial HSYNC2VSYNC_POL_CTRL: 0x%x\n", regresp.val);
-		if ((regresp.val & 0x3) != 0)
-			__raw_writel(0x4, hdmi_csr_base);
-	}
-#endif
-	/*regresp.val &= ~0x03; // clear HSP and VSP bits */
-	/*debug("Final HSYNC2VSYNC_POL_CTRL: 0x%x\n",regresp.val); */
-	/*CDN_API_General_Write_Register_blocking(ADDR_DPTX_FRAMER +
-						(DP_FRAMER_SP << 2),
-						regresp.val); */
 
 	udelay(20000);
 
 	return 0;
 }
 
-int imx8_hdmi_enable(int encoding,
-		      struct video_mode_settings *vms)
+static int imx8m_hdmi_update_timings(struct udevice *dev)
 {
-	int vic = 0;
-	const int use_phy_pixel_clk = 1;
+	struct imx8m_hdmi_priv *priv = dev_get_priv(dev);
 
 	/* map the resolution to a VIC index in the vic table*/
-	if ((vms->xres == 1280) && (vms->yres == 720))
-		vic = 1; /* 720p60 */
-	else if ((vms->xres == 1920) && (vms->yres == 1080))
-		vic = 2; /* 1080p60 */
-	else if ((vms->xres == 3840) && (vms->yres == 2160))
-		vic = 3; /* 2160p60 */
-	else /* if  ((vms->xres == 720) && (vms->yres == 480)) */
-		vic = 0; /* 480p60 */
+	if ((priv->timings.hactive.typ == 1280) && (priv->timings.vactive.typ == 720))
+		priv->vic = 1; /* 720p60 */
+	else if ((priv->timings.hactive.typ == 1920) && (priv->timings.vactive.typ == 1080))
+		priv->vic = 2; /* 1080p60 */
+	else if ((priv->timings.hactive.typ == 3840) && (priv->timings.vactive.typ == 2160))
+		priv->vic = 3; /* 2160p60 */
+	else
+		priv->vic = 0; /* 480p60 */
 
-	imx8_hdmi_set_vic_mode(vic, vms);
-	return imx8_hdmi_init(vic, encoding, g_color_depth, use_phy_pixel_clk);
+	return imx8m_hdmi_set_vic_mode(priv->vic, priv);
 }
 
-void imx8_hdmi_disable(void)
+static void imx8m_hdmi_disable(void)
 {
 	int ret;
 	GENERAL_READ_REGISTER_RESPONSE resp;
@@ -279,11 +213,9 @@ void imx8_hdmi_disable(void)
 	if (ret != CDN_OK) {
 		printf("%s(): dn_api_general_read_register_blocking failed\n",
 		       __func__);
-		/*return;*/
 	}
 
 	resp.val &= ~F_DATA_EN(1); /* disable HDMI */
-	/*resp.val |= F_SET_AVMUTE( 1);*/
 
 	ret = cdn_api_general_write_register_blocking(ADDR_SOURCE_MHL_HD +
 						      (HDTX_CONTROLLER << 2),
@@ -294,3 +226,87 @@ void imx8_hdmi_disable(void)
 		return;
 	}
 }
+
+static int imx8m_hdmi_read_timing(struct udevice *dev, struct display_timing *timing)
+{
+	struct imx8m_hdmi_priv *priv = dev_get_priv(dev);
+
+	if (timing) {
+		memcpy(timing, &priv->timings, sizeof(struct display_timing));
+
+		if (priv->hpol)
+			timing->flags |= DISPLAY_FLAGS_HSYNC_HIGH;
+
+		if (priv->vpol)
+			timing->flags |= DISPLAY_FLAGS_VSYNC_HIGH;
+
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
+static int imx8m_hdmi_enable(struct udevice *dev, int panel_bpp,
+		      const struct display_timing *timing)
+{
+	struct imx8m_hdmi_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	ret = imx8m_hdmi_init(priv->vic, 1, 8, true);
+	if (ret) {
+		printf("HDMI enable failed, ret %d!\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int imx8m_hdmi_probe(struct udevice *dev)
+{
+	struct imx8m_hdmi_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	printf("%s\n", __func__);
+
+	priv->base = dev_read_addr(dev);
+	if (priv->base == FDT_ADDR_T_NONE)
+		return -EINVAL;
+
+	ret = video_link_get_display_timings(&priv->timings);
+	if (ret) {
+		printf("decode display timing error %d\n", ret);
+		return ret;
+	}
+
+	imx8m_hdmi_update_timings(dev);
+
+	return 0;
+}
+
+static int imx8m_hdmi_remove(struct udevice *dev)
+{
+	imx8m_hdmi_disable();
+
+	return 0;
+}
+
+struct dm_display_ops imx8m_hdmi_ops = {
+	.read_timing =  imx8m_hdmi_read_timing,
+	.enable =  imx8m_hdmi_enable,
+};
+
+static const struct udevice_id  imx8m_hdmi_ids[] = {
+	{ .compatible = "fsl,imx8mq-hdmi" },
+	{ }
+};
+
+U_BOOT_DRIVER( imx8m_hdmi) = {
+	.name				= " imx8m_hdmi",
+	.id				= UCLASS_DISPLAY,
+	.of_match			=  imx8m_hdmi_ids,
+	.bind				= dm_scan_fdt_dev,
+	.probe				=  imx8m_hdmi_probe,
+	.remove				= imx8m_hdmi_remove,
+	.ops				= & imx8m_hdmi_ops,
+	.priv_auto_alloc_size		= sizeof(struct  imx8m_hdmi_priv),
+};

rafluan/u-boot-fslc

(cherry picked from commit 3188ceb5ab53627769f028ad9589e0fd0d5da406)
(cherry picked from commit a132cd18303034757e7c5ae2d6733e77523edb08)
(cherry picked from commit 5a9eb87342e16d8f8deb39e5a963a5be08941dbc)
(cherry picked from commit 68a1a6619d53dc5cd34bbc8984525a4a6bad3a71)

drivers/video/imx/hdmi/imx8m_hdmi.c
@@ -1,14 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright 2018 NXP
+ * Copyright 2019 NXP
  *
- * SPDX-License-Identifier:	GPL-2.0+
  */
 
 #include <common.h>
-#include <command.h>
-#include <asm/mach-imx/video.h>
-#include <asm/arch/video_common.h>
-#include <imx8_hdmi.h>
+#include <clk.h>
+#include <dm.h>
+#include <display.h>
+#include <video.h>
+#include <asm/io.h>
+#include <dm/device-internal.h>
+#include <linux/iopoll.h>
+#include <clk.h>
+#include <video_link.h>
 
 #include "API_General.h"
 #include "vic_table.h"
@@ -23,29 +28,19 @@
 #include "source_vif.h"
 #include "general_handler.h"
 #include "mhl_hdtx_top.h"
-
-
-#ifdef CONFIG_IMX8QM
-#include "API_AFE_mcu1_dp.h"
-#include "API_AFE_ss28fdsoi_kiran_hdmitx.h"
-#endif
-
-#ifdef CONFIG_IMX8M
 #include "API_AFE_t28hpc_hdmitx.h"
-#endif
 
-DECLARE_GLOBAL_DATA_PTR;
+struct imx8m_hdmi_priv {
+	fdt_addr_t base;
+	struct display_timing timings;
+	int vic;
+	bool hpol;
+	bool vpol;
+};
 
-#define ON  1
-#define OFF 0
-
-unsigned long g_encoding = 1;    /* 1 RGB, 2 YUV 444, 4 YUV 422, 8 YUV 420 */
-unsigned long g_color_depth = 8; /* 8 pits per color */
-
-static int imx8_hdmi_set_vic_mode(int vic,
-				  struct video_mode_settings *vms)
+static int imx8m_hdmi_set_vic_mode(int vic,
+				  struct imx8m_hdmi_priv *priv)
 {
-	/*struct video_mode_settings *vms = &vm_settings[VM_USER]; */
 	uint32_t pixel_clock_kHz;
 	uint32_t frame_rate_Hz;
 	uint32_t frame_rate_frac_Hz;
@@ -57,18 +52,17 @@ static int imx8_hdmi_set_vic_mode(int vic,
 		return -1;
 	}
 
+	priv->timings.hfront_porch.typ = vic_table[vic][FRONT_PORCH];
+	priv->timings.hback_porch.typ = vic_table[vic][BACK_PORCH];
+	priv->timings.hsync_len.typ = vic_table[vic][HSYNC];
+	priv->timings.vfront_porch.typ = vic_table[vic][TYPE_EOF];
+	priv->timings.vback_porch.typ = vic_table[vic][SOF];
+	priv->timings.vsync_len.typ = vic_table[vic][VSYNC];
+	priv->timings.hactive.typ = vic_table[vic][H_ACTIVE];
+	priv->timings.vactive.typ = vic_table[vic][V_ACTIVE];
 
-	vms->hfp   = vic_table[vic][FRONT_PORCH];
-	vms->hbp   = vic_table[vic][BACK_PORCH];
-	vms->hsync = vic_table[vic][HSYNC];
-	vms->vfp   = vic_table[vic][TYPE_EOF];
-	vms->vbp   = vic_table[vic][SOF];
-	vms->vsync = vic_table[vic][VSYNC];
-	vms->xres  = vic_table[vic][H_ACTIVE];
-	vms->yres  = vic_table[vic][V_ACTIVE];
-
-	vms->hpol = vic_table[vic][HSYNC_POL] != 0;
-	vms->vpol = vic_table[vic][VSYNC_POL] != 0;
+	priv->hpol = vic_table[vic][HSYNC_POL] != 0;
+	priv->vpol = vic_table[vic][VSYNC_POL] != 0;
 
 	cea_vic = vic_table[vic][VIC];
 	if (vic_table[vic][I_P] != 0)
@@ -80,40 +74,29 @@ static int imx8_hdmi_set_vic_mode(int vic,
 	frame_rate_frac_Hz = frame_rate_Hz % 1000;
 	frame_rate_Hz      /= 1000;
 
-	vms->pixelclock = pixel_clock_kHz;
+	priv->timings.pixelclock.typ = pixel_clock_kHz * 1000;
 
 	debug("Cadence VIC %3d, CEA VIC %3d: %4d x %4d %c @ %3d.%03d [%6d kHz] Vpol=%d Hpol=%d\n",
-	      vic, cea_vic, vms->xres, vms->yres, iflag, frame_rate_Hz,
-	      frame_rate_frac_Hz, pixel_clock_kHz, vms->vpol, vms->hpol);
+	      vic, cea_vic, priv->timings.hactive.typ, priv->timings.vactive.typ, iflag, frame_rate_Hz,
+	      frame_rate_frac_Hz, pixel_clock_kHz, priv->vpol, priv->hpol);
 
 	debug(" mode timing fp sync bp h:%3d %3d %3d v:%3d %3d %3d\n",
-	      vms->hfp, vms->hsync, vms->hbp, vms->vfp, vms->vsync, vms->vbp);
+	      priv->timings.hfront_porch.typ, priv->timings.hsync_len.typ, priv->timings.hback_porch.typ,
+	      priv->timings.vfront_porch.typ, priv->timings.vsync_len.typ, priv->timings.vback_porch.typ);
 
 	return 0;
-	/*debug("leaving %s() ...\n", __func__); */
 }
 
-static int imx8_hdmi_init(int vic,
+static int imx8m_hdmi_init(int vic,
 			  int encoding,
 			  int color_depth,
 			  bool pixel_clk_from_phy)
 {
 	int ret;
-#ifdef CONFIG_IMX8QM
-	sc_ipc_t ipcHndl = gd->arch.ipc_channel_handle;
-	void __iomem *hdmi_csr_base = (void __iomem *)0x56261000;
-#endif
-	/*GENERAL_Read_Register_response regresp; */
-	/*uint8_t sts; */
 	uint32_t character_freq_khz;
 
 	uint8_t echo_msg[] = "echo test";
 	uint8_t echo_resp[sizeof(echo_msg) + 1];
-	/*uint8_t response; */
-	/*uint8_t dpcd_resp; */
-	/*uint8_t hdcp_resp; */
-	/*uint8_t capb_resp; */
-	/*uint32_t temp; */
 
 	/*================================================================== */
 	/* Parameterization: */
@@ -147,22 +130,6 @@ static int imx8_hdmi_init(int vic,
 	/*================================================================== */
 	/* Parameterization done */
 	/*================================================================== */
-#ifdef CONFIG_IMX8QM
-	/* set the pixel link mode and pixel type */
-	SC_MISC_SET_CONTROL(ipcHndl, SC_R_HDMI, SC_C_PHY_RESET, 0);
-#if 1
-	SC_MISC_SET_CONTROL(ipcHndl, SC_R_DC_0, SC_C_PXL_LINK_MST1_ADDR, 1);
-	/*SC_MISC_SET_CONTROL(ipcHndl, SC_R_DC_0, SC_C_PXL_LINK_MST1_ADDR, 0);*/
-	if (g_clock_mode == CLOCK_MODES_HDMI_DUAL) {
-		SC_MISC_SET_CONTROL(ipcHndl, SC_R_DC_0,
-				    SC_C_PXL_LINK_MST2_ADDR, 2);
-		/*SC_MISC_SET_CONTROL(ipcHndl, SC_R_DC_0,
-		  SC_C_PXL_LINK_MST2_ADDR, 0); */
-		__raw_writel(0x6, hdmi_csr_base);
-	} else
-#endif
-		__raw_writel(0x34, hdmi_csr_base);
-#endif
 	cdn_api_init();
 	debug("CDN_API_Init completed\n");
 
@@ -185,27 +152,15 @@ static int imx8_hdmi_init(int vic,
 	debug("phy_cfg_t28hpc (character_freq_mhz = %d)\n",
 	      character_freq_khz);
 
-	/*phy_reset(1); */
-
-#ifdef CONFIG_IMX8QM
-	SC_MISC_SET_CONTROL(ipcHndl, SC_R_HDMI, SC_C_PHY_RESET, 1);
-#endif
 	hdmi_tx_t28hpc_power_config_seq(4);
-#ifdef CONFIG_IMX8QM
-	/* Set the lane swapping */
-	ret = cdn_api_general_write_register_blocking
-		(ADDR_SOURCD_PHY + (LANES_CONFIG << 2),
-		 F_SOURCE_PHY_LANE0_SWAP(3) | F_SOURCE_PHY_LANE1_SWAP(0) |
-		 F_SOURCE_PHY_LANE2_SWAP(1) | F_SOURCE_PHY_LANE3_SWAP(2) |
-		 F_SOURCE_PHY_COMB_BYPASS(0) | F_SOURCE_PHY_20_10(1));
-#else
+
 	/* Set the lane swapping */
 	ret = cdn_api_general_write_register_blocking
 		(ADDR_SOURCD_PHY + (LANES_CONFIG << 2),
 		 F_SOURCE_PHY_LANE0_SWAP(0) | F_SOURCE_PHY_LANE1_SWAP(1) |
 		 F_SOURCE_PHY_LANE2_SWAP(2) | F_SOURCE_PHY_LANE3_SWAP(3) |
 		 F_SOURCE_PHY_COMB_BYPASS(0) | F_SOURCE_PHY_20_10(1));
-#endif
+
 	debug("_General_Write_Register_blocking LANES_CONFIG ret = %d\n", ret);
 
 	ret = CDN_API_HDMITX_Init_blocking();
@@ -223,51 +178,30 @@ static int imx8_hdmi_init(int vic,
 	ret =  CDN_API_HDMITX_SetVic_blocking(vic_mode, bps, format);
 	debug("CDN_API_HDMITX_SetVic_blocking ret = %d\n", ret);
 
-#ifdef CONFIG_IMX8QM
-	{
-		GENERAL_Read_Register_response regresp;
-		/* adjust the vsync/hsync polarity */
-		cdn_api_general_read_register_blocking(ADDR_SOURCE_VIF +
-						       (HSYNC2VSYNC_POL_CTRL
-							<< 2),
-						       &regresp);
-		debug("Initial HSYNC2VSYNC_POL_CTRL: 0x%x\n", regresp.val);
-		if ((regresp.val & 0x3) != 0)
-			__raw_writel(0x4, hdmi_csr_base);
-	}
-#endif
-	/*regresp.val &= ~0x03; // clear HSP and VSP bits */
-	/*debug("Final HSYNC2VSYNC_POL_CTRL: 0x%x\n",regresp.val); */
-	/*CDN_API_General_Write_Register_blocking(ADDR_DPTX_FRAMER +
-						(DP_FRAMER_SP << 2),
-						regresp.val); */
 
 	udelay(20000);
 
 	return 0;
 }
 
-int imx8_hdmi_enable(int encoding,
-		      struct video_mode_settings *vms)
+static int imx8m_hdmi_update_timings(struct udevice *dev)
 {
-	int vic = 0;
-	const int use_phy_pixel_clk = 1;
+	struct imx8m_hdmi_priv *priv = dev_get_priv(dev);
 
 	/* map the resolution to a VIC index in the vic table*/
-	if ((vms->xres == 1280) && (vms->yres == 720))
-		vic = 1; /* 720p60 */
-	else if ((vms->xres == 1920) && (vms->yres == 1080))
-		vic = 2; /* 1080p60 */
-	else if ((vms->xres == 3840) && (vms->yres == 2160))
-		vic = 3; /* 2160p60 */
-	else /* if  ((vms->xres == 720) && (vms->yres == 480)) */
-		vic = 0; /* 480p60 */
+	if ((priv->timings.hactive.typ == 1280) && (priv->timings.vactive.typ == 720))
+		priv->vic = 1; /* 720p60 */
+	else if ((priv->timings.hactive.typ == 1920) && (priv->timings.vactive.typ == 1080))
+		priv->vic = 2; /* 1080p60 */
+	else if ((priv->timings.hactive.typ == 3840) && (priv->timings.vactive.typ == 2160))
+		priv->vic = 3; /* 2160p60 */
+	else
+		priv->vic = 0; /* 480p60 */
 
-	imx8_hdmi_set_vic_mode(vic, vms);
-	return imx8_hdmi_init(vic, encoding, g_color_depth, use_phy_pixel_clk);
+	return imx8m_hdmi_set_vic_mode(priv->vic, priv);
 }
 
-void imx8_hdmi_disable(void)
+static void imx8m_hdmi_disable(void)
 {
 	int ret;
 	GENERAL_READ_REGISTER_RESPONSE resp;
@@ -279,11 +213,9 @@ void imx8_hdmi_disable(void)
 	if (ret != CDN_OK) {
 		printf("%s(): dn_api_general_read_register_blocking failed\n",
 		       __func__);
-		/*return;*/
 	}
 
 	resp.val &= ~F_DATA_EN(1); /* disable HDMI */
-	/*resp.val |= F_SET_AVMUTE( 1);*/
 
 	ret = cdn_api_general_write_register_blocking(ADDR_SOURCE_MHL_HD +
 						      (HDTX_CONTROLLER << 2),
@@ -294,3 +226,85 @@ void imx8_hdmi_disable(void)
 		return;
 	}
 }
+
+static int imx8m_hdmi_read_timing(struct udevice *dev, struct display_timing *timing)
+{
+	struct imx8m_hdmi_priv *priv = dev_get_priv(dev);
+
+	if (timing) {
+		memcpy(timing, &priv->timings, sizeof(struct display_timing));
+
+		if (priv->hpol)
+			timing->flags |= DISPLAY_FLAGS_HSYNC_HIGH;
+
+		if (priv->vpol)
+			timing->flags |= DISPLAY_FLAGS_VSYNC_HIGH;
+
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
+static int imx8m_hdmi_enable(struct udevice *dev, int panel_bpp,
+		      const struct display_timing *timing)
+{
+	struct imx8m_hdmi_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	ret = imx8m_hdmi_init(priv->vic, 1, 8, true);
+	if (ret) {
+		printf("HDMI enable failed, ret %d!\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int imx8m_hdmi_probe(struct udevice *dev)
+{
+	struct imx8m_hdmi_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	priv->base = dev_read_addr(dev);
+	if (priv->base == FDT_ADDR_T_NONE)
+		return -EINVAL;
+
+	ret = video_link_get_display_timings(&priv->timings);
+	if (ret) {
+		printf("decode display timing error %d\n", ret);
+		return ret;
+	}
+
+	imx8m_hdmi_update_timings(dev);
+
+	return 0;
+}
+
+static int imx8m_hdmi_remove(struct udevice *dev)
+{
+	imx8m_hdmi_disable();
+
+	return 0;
+}
+
+struct dm_display_ops imx8m_hdmi_ops = {
+	.read_timing =  imx8m_hdmi_read_timing,
+	.enable =  imx8m_hdmi_enable,
+};
+
+static const struct udevice_id  imx8m_hdmi_ids[] = {
+	{ .compatible = "fsl,imx8mq-hdmi" },
+	{ }
+};
+
+U_BOOT_DRIVER( imx8m_hdmi) = {
+	.name				= " imx8m_hdmi",
+	.id				= UCLASS_DISPLAY,
+	.of_match			=  imx8m_hdmi_ids,
+	.bind				= dm_scan_fdt_dev,
+	.probe				=  imx8m_hdmi_probe,
+	.remove				= imx8m_hdmi_remove,
+	.ops				= & imx8m_hdmi_ops,
+	.priv_auto		= sizeof(struct  imx8m_hdmi_priv),
+};