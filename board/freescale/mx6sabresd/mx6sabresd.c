nxp-imx/uboot-imx

MLK-16753-6 imx: mx6sabresd: add optee support
Add different defconfigs
Enable Trustzone.
Update env to runtime boot OP-TEE.
To 6QP SDB, TZASC enabled, need board rework and new ddr script.

Signed-off-by: Peng Fan <peng.fan@nxp.com>

board/freescale/mx6sabresd/mx6sabresd.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012-2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
  *
  * Author: Fabio Estevam <fabio.estevam@freescale.com>
  *
@@ -1338,6 +1339,11 @@ int board_late_init(void)
 {
 #ifdef CONFIG_CMD_BMODE
 	add_board_boot_modes(board_boot_modes);
+#endif
+
+	setenv("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	setenv("tee", "yes");
 #endif
 
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG

rafluan/u-boot-fslc

(cherry picked from commit 2db3156d89ad13133fb1444186e906a5db178913)
(cherry picked from commit a1370b6768c0ce71555ac056bca94102e8a7228e)
(cherry picked from commit 9d483ef0ef1c44f5067405092b738863d5e07489)
(cherry picked from commit 0f79d0ae9d22406b7b1189b9d26e5b26e6c09b4b)
(cherry picked from commit 94af854f2ba1cf8bdae8717218f23f3abd6de5b0)
(cherry picked from commit c15e8269ae7193394de870c8d63c93f38cf7dbec)
(cherry picked from commit 77f4db659c5088a6c15ea8681534372860c8417b)

board/freescale/mx6sabresd/mx6sabresd.c
@@ -969,6 +969,11 @@ int board_late_init(void)
 	add_board_boot_modes(board_boot_modes);
 #endif
 
+	env_set("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	env_set("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 	env_set("board_name", "SABRESD");
 