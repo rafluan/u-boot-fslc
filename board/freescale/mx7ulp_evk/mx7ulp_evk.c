nxp-imx/uboot-imx

MLK-14445-6 mx7ulp_evk: Add USB OTG0 support
Porting codes to support USB OTG0 on the EVK board. Convert
to use DM USB driver.

Signed-off-by: Ye Li <ye.li@nxp.com>

board/freescale/mx7ulp_evk/mx7ulp_evk.c
@@ -9,11 +9,15 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/mx7ulp-pins.h>
 #include <asm/arch/iomux.h>
+#include <asm/gpio.h>
+#include <usb.h>
+#include <dm.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 #define UART_PAD_CTRL	(PAD_CTL_PUS_UP)
 #define QSPI_PAD_CTRL1	(PAD_CTL_PUS_UP | PAD_CTL_DSE)
+#define OTG_ID_GPIO_PAD_CTRL	(PAD_CTL_IBE_ENABLE)
 
 int dram_init(void)
 {
@@ -63,6 +67,37 @@ int board_qspi_init(void)
 }
 #endif
 
+#ifdef CONFIG_DM_USB
+static iomux_cfg_t const usb_otg1_pads[] = {
+	MX7ULP_PAD_PTC8__PTC8 | MUX_PAD_CTRL(OTG_ID_GPIO_PAD_CTRL),  /* gpio for OTG ID*/
+};
+
+static void setup_usb(void)
+{
+	mx7ulp_iomux_setup_multiple_pads(usb_otg1_pads,
+						 ARRAY_SIZE(usb_otg1_pads));
+
+	gpio_request(IMX_GPIO_NR(3, 8), "otg_id");
+	gpio_direction_input(IMX_GPIO_NR(3, 8));
+}
+
+int board_ehci_usb_phy_mode(struct udevice *dev)
+{
+	int ret = 0;
+
+	if (dev_get_addr(dev) == USBOTG0_RBASE) {
+		ret = gpio_get_value(IMX_GPIO_NR(3, 8));
+
+		if (ret)
+			return USB_INIT_DEVICE;
+		else
+			return USB_INIT_HOST;
+	}
+
+	return USB_INIT_HOST;
+}
+#endif
+
 int board_early_init_f(void)
 {
 	setup_iomux_uart();
@@ -77,6 +112,10 @@ int board_init(void)
 
 #ifdef CONFIG_FSL_QSPI
 	board_qspi_init();
+#endif
+
+#ifdef CONFIG_DM_USB
+	setup_usb();
 #endif
 
 	return 0;

rafluan/u-boot-fslc

(cherry picked from commit b4e01a67a0740c524e7522da7ace0488f86261db)
(cherry picked from commit 23144983d3347f8f91695cea73aab8558cc66629)
(cherry picked from commit a5d3e7a334689b5c7b4e99f10c1a6044e4775699)
(cherry picked from commit 4906e4b9ff0e75ad517fa3ed3d49430d3533d3f6)
(cherry picked from commit c315588cf84d2a4b61ad11827444891edf17a35a)
(cherry picked from commit fef193218f24baee61479b3c551e2b0d376cc5bc)
(cherry picked from commit 420de7f8c1861a6bb2109ecd3ac01d099797f700)

board/freescale/mx7ulp_evk/mx7ulp_evk.c
@@ -12,11 +12,15 @@
 #include <asm/arch/mx7ulp-pins.h>
 #include <asm/arch/iomux.h>
 #include <asm/mach-imx/boot_mode.h>
+#include <asm/gpio.h>
+#include <usb.h>
+#include <dm.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 #define UART_PAD_CTRL	(PAD_CTL_PUS_UP)
 #define QSPI_PAD_CTRL1	(PAD_CTL_PUS_UP | PAD_CTL_DSE)
+#define OTG_ID_GPIO_PAD_CTRL	(PAD_CTL_IBE_ENABLE)
 
 int dram_init(void)
 {
@@ -66,6 +70,37 @@ int board_qspi_init(void)
 }
 #endif
 
+#ifdef CONFIG_DM_USB
+static iomux_cfg_t const usb_otg1_pads[] = {
+	MX7ULP_PAD_PTC8__PTC8 | MUX_PAD_CTRL(OTG_ID_GPIO_PAD_CTRL),  /* gpio for OTG ID*/
+};
+
+static void setup_usb(void)
+{
+	mx7ulp_iomux_setup_multiple_pads(usb_otg1_pads,
+						 ARRAY_SIZE(usb_otg1_pads));
+
+	gpio_request(IMX_GPIO_NR(3, 8), "otg_id");
+	gpio_direction_input(IMX_GPIO_NR(3, 8));
+}
+
+int board_ehci_usb_phy_mode(struct udevice *dev)
+{
+	int ret = 0;
+
+	if (devfdt_get_addr(dev) == USBOTG0_RBASE) {
+		ret = gpio_get_value(IMX_GPIO_NR(3, 8));
+
+		if (ret)
+			return USB_INIT_DEVICE;
+		else
+			return USB_INIT_HOST;
+	}
+
+	return USB_INIT_HOST;
+}
+#endif
+
 int board_early_init_f(void)
 {
 	setup_iomux_uart();
@@ -80,6 +115,10 @@ int board_init(void)
 
 #ifdef CONFIG_FSL_QSPI
 	board_qspi_init();
+#endif
+
+#ifdef CONFIG_DM_USB
+	setup_usb();
 #endif
 
 	return 0;