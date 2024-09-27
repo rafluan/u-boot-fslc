nxp-imx/uboot-imx

LFU-616-1 spl: mmc: Change the h_spl_load_read to weak
Change the MMC read function h_spl_load_read to weak, so we can
override it with private implementation.

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

common/spl/spl_mmc.c
@@ -59,7 +59,7 @@ static int mmc_load_legacy(struct spl_image_info *spl_image,
 	return 0;
 }
 
-ulong h_spl_load_read(struct spl_load_info *load, ulong sector,
+ulong __weak h_spl_load_read(struct spl_load_info *load, ulong sector,
 			     ulong count, void *buf)
 {
 	struct mmc *mmc = load->dev;

rafluan/u-boot-fslc

common/spl/spl_mmc.c
@@ -19,7 +19,7 @@
 #include <image.h>
 #include <imx_container.h>
 
-static ulong h_spl_load_read(struct spl_load_info *load, ulong off,
+ulong __weak h_spl_load_read(struct spl_load_info *load, ulong off,
 			     ulong size, void *buf)
 {
 	struct blk_desc *bd = load->priv;