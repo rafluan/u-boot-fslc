nxp-imx/uboot-imx

MLK-20945-7 imx8qm/qxp: Add MEK FSPI configurations
Add MEK FSPI configs to enable SPL SPI and NOR support for flexspi booting

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>
(cherry picked from commit f303f969ebb0f525c413c47519725275d00025a1)

include/configs/imx8qxp_mek.h
@@ -14,6 +14,12 @@
 #define CONFIG_SYS_MONITOR_LEN				(1024 * 1024)
 #define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_USE_SECTOR
 #define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR		0x1040 /* (32K + 2Mb)/sector_size */
+
+/*
+ * 0x08081000 - 0x08180FFF is for m4_0 xip image,
+  * So 3rd container image may start from 0x8181000
+ */
+#define CONFIG_SYS_UBOOT_BASE 0x08181000
 #define CONFIG_SYS_MMCSD_FS_BOOT_PARTITION		0
 
 #define CONFIG_SPL_LDSCRIPT		"arch/arm/cpu/armv8/u-boot-spl.lds"

rafluan/u-boot-fslc

(cherry picked from commit b6c7ad55b757be56d454f0d64667c7547e3690dc)
(cherry picked from commit 8043244218c1c0391655b8f2c3f3b08bed741f0f)
(cherry picked from commit 8768765e5829ecdd8d7e8ee1d8c60abaf67528fb)

include/configs/imx8qxp_mek.h
@@ -14,6 +14,12 @@
 
 #ifdef CONFIG_SPL_BUILD
 #define CFG_MALLOC_F_ADDR		0x00138000
+
+/*
+ * 0x08081000 - 0x08180FFF is for m4_0 xip image,
+  * So 3rd container image may start from 0x8181000
+ */
+#define CFG_SYS_UBOOT_BASE 0x08181000
 #endif
 
 #ifdef CONFIG_AHAB_BOOT